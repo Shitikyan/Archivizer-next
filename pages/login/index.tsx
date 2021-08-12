6ed7718ce6ab286449be0883379d81417daf0c18
Karen-Avdalyan <karenavdalian@gmail.com>
2021-08-12 14:29:56 +0400

    fixed the breaking of styles

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { TextField, Button } from '@material-ui/core';
 import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import clsx from 'clsx';
@@ -17,6 +18,20 @@ interface ILoginFormInputs {
   agreement: boolean;
 }
 
+const useStyles = makeStyles({
+  checkboxRoot: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+  },
+  signInButtonRoot: {
+    padding: '8px 16px',
+    textTransform: 'none',
+    fontFamily: 'Rubik',
+  }
+});
+
 const Login = () => {
   const {
     handleSubmit,
@@ -24,6 +39,8 @@ const Login = () => {
     formState: { errors },
   } = useForm<ILoginFormInputs>();
   const onSubmit: SubmitHandler<ILoginFormInputs> = (data) => console.log(data);
+
+  const classes = useStyles();
 
   return (
     <div className={styles.loginContainer}>
@@ -53,91 +70,101 @@ const Login = () => {
         className={`${styles.form} flex px-10`}
         onSubmit={handleSubmit(onSubmit)}
       >
-        <label
-          className='block text-sm font-medium text-gray-700 dark:text-gray-300'
-          htmlFor='emailAddress'
-        >
-          Email address
-        </label>
-        <Controller
-          name='emailAddress'
-          control={control}
-          defaultValue=''
-          rules={{
-            required: true,
-            pattern:
-              /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-          }}
-          render={({ field }) => (
-            <TextField
-              id='emailAddress'
-              error={!!errors.emailAddress}
-              helperText={
-                errors.emailAddress?.type == 'required'
-                  ? 'This field is required'
-                  : errors.emailAddress?.type == 'pattern'
-                  ? 'Invalid email address'
-                  : null
-              }
-              size='small'
-              variant='outlined'
-              {...field}
-            />
-          )}
-        />
-        <label
-          className='block text-sm font-medium text-gray-700 dark:text-gray-300'
-          htmlFor='password'
-        >
-          Password
-        </label>
-        <Controller
-          name='password'
-          control={control}
-          defaultValue=''
-          rules={{ required: true }}
-          render={({ field }) => (
-            <TextField
-              id='password'
-              type='password'
-              error={!!errors.password}
-              helperText={
-                errors.password?.type == 'required' && 'This field is required'
-              }
-              variant='outlined'
-              size='small'
-              {...field}
-            />
-          )}
-        />
-        {errors.password && (
-          <span className='text-red-500'>This field is required</span>
-        )}
-        <div style={{ marginTop: 10 }}>
-          <Link
-            href='default.asp'
-            target='_blank'
-            className='hover:text-blue-500 font-semibold flex justify-end'
+        <div>
+          <label
+            className='block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1'
+            htmlFor='emailAddress'
           >
-            Forgot your password?
-          </Link>
+            Email address
+          </label>
+          <Controller
+            name='emailAddress'
+            control={control}
+            defaultValue=''
+            rules={{
+              required: true,
+              pattern:
+                /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            }}
+            render={({ field }) => (
+              <TextField
+                id='emailAddress'
+                error={!!errors.emailAddress}
+                helperText={
+                  errors.emailAddress?.type == 'required'
+                    ? 'This field is required'
+                    : errors.emailAddress?.type == 'pattern'
+                    ? 'Invalid email address'
+                    : null
+                }
+                fullWidth
+                size='small'
+                variant='outlined'
+                {...field}
+              />
+            )}
+          />
         </div>
+        <div>
+          <label
+            className='block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1'
+            htmlFor='password'
+          >
+            Password
+          </label>
+          <Controller
+            name='password'
+            control={control}
+            defaultValue=''
+            rules={{ required: true }}
+            render={({ field }) => (
+              <TextField
+                id='password'
+                type='password'
+                error={!!errors.password}
+                helperText={
+                  errors.password?.type == 'required' &&
+                  'This field is required'
+                }
+                fullWidth
+                variant='outlined'
+                size='small'
+                {...field}
+              />
+            )}
+          />
+        </div>
+        <Link
+          href='default.asp'
+          target='_blank'
+          className='hover:text-blue-500 font-medium flex justify-end'
+        >
+          Forgot your password?
+        </Link>
         <div className='flex'>
           <Controller
             name='agreement'
             control={control}
             rules={{ required: true }}
             render={({ field }) => (
-              <Checkbox color='primary' id='agreement' {...field} />
+              <Checkbox
+                id='agreement'
+                color='primary'
+                className={classes.checkboxRoot}
+                {...field}
+              />
             )}
           />
-          <label htmlFor='agreement' style={{ marginTop: 7 }}>
+          <label
+            htmlFor='agreement'
+            className='ml-2 block text-sm text-gray-900 dark:text-gray-100'
+          >
             <div className='my-22'>
               I confirm that I have read and agree to{' '}
               <Link
                 href='default.asp'
                 target='_blank'
-                className='hover:text-blue-500 font-bold'
+                className='font-medium hover:text-blue-500'
               >
                 Payment
               </Link>
@@ -146,7 +173,7 @@ const Login = () => {
                 <Link
                   href='default.asp'
                   target='_blank'
-                  className='hover:text-blue-500 font-bold'
+                  className='font-medium hover:text-blue-500'
                 >
                   Refund
                 </Link>{' '}
@@ -155,7 +182,7 @@ const Login = () => {
                   <Link
                     href='default.asp'
                     target='_blank'
-                    className='hover:text-blue-500 font-bold'
+                    className='hover:text-blue-500 font-medium'
                   >
                     Privacy
                   </Link>
@@ -169,7 +196,7 @@ const Login = () => {
           variant='contained'
           type='submit'
           color='primary'
-          className={clsx('whitespace-nowrap w-full', styles.signinButton)}
+          className={clsx('whitespace-nowrap w-full', classes.signInButtonRoot)}
         >
           Sign in
         </Button>
