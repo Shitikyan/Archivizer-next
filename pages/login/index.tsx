39553dfb4173bdb26ab9ec950d3dbfdbd5649abd
SergeyGhazaryan <sergey.ghazaryan2000@gmail.com>
2021-08-13 11:32:37 +0400

    created payment dialog

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -12,6 +12,7 @@ import { PasswordInput } from '../../shared/Form/PasswordInput';
 import { Divider } from '../../shared/Divider';
 import { login } from '../../services/auth';
 import { Link } from '../../shared/Link';
+import { Dialog } from '../../shared/Dialog';
 
 import styles from './styles.module.css';
 
@@ -37,6 +38,7 @@ const useStyles = makeStyles({
 
 const Login = () => {
   const [formData, setFormData] = useState<ILoginFormInputs | null>(null);
+  const [paymentDialogVisible, setPaymentDialogVisible] = useState(false);
 
   const classes = useStyles();
   const {
@@ -62,6 +64,10 @@ const Login = () => {
   }, [data]);
 
   const onSubmit: SubmitHandler<ILoginFormInputs> = (data) => setFormData(data);
+
+  const paymentDialogEvent = () => {
+    setPaymentDialogVisible(!paymentDialogVisible);
+  };
 
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
@@ -176,9 +182,8 @@ const Login = () => {
               <div className='my-22'>
                 I confirm that I have read and agree to{' '}
                 <Link
-                  href='default.asp'
-                  target='_blank'
                   className='font-medium hover:text-blue-500'
+                  onClick={paymentDialogEvent}
                 >
                   Payment
                 </Link>
@@ -204,6 +209,63 @@ const Login = () => {
                   Policies
                 </div>
               </div>
+              <Dialog
+                title='Payment Terms and Policy'
+                isOpen={paymentDialogVisible}
+                onCancel={paymentDialogEvent}
+                onSave={paymentDialogEvent}
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum. Why do we use it? It is a long established fact
+                that a reader will be distracted by the readable content of a
+                page when looking at its layout. The point of using Lorem Ipsum
+                is that it has a more-or-less normal distribution of letters, as
+                opposed to using 'Content here, content here', making it look
+                like readable English. Many desktop publishing packages and web
+                page editors now use Lorem Ipsum as their default model text,
+                and a search for 'lorem ipsum' will uncover many web sites still
+                in their infancy. Various versions have evolved over the years,
+                sometimes by accident, sometimes on purpose (injected humour and
+                the like). Where does it come from? Contrary to popular belief,
+                Lorem Ipsum is not simply random text. It has roots in a piece
+                of classical Latin literature from 45 BC, making it over 2000
+                years old. Richard McClintock, a Latin professor at
+                Hampden-Sydney College in Virginia, looked up one of the more
+                obscure Latin words, consectetur, from a Lorem Ipsum passage,
+                and going through the cites of the word in classical literature,
+                discovered the undoubtable source. Lorem Ipsum comes from
+                sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
+                (The Extremes of Good and Evil) by Cicero, written in 45 BC.
+                This book is a treatise on the theory of ethics, very popular
+                during the Renaissance. The first line of Lorem Ipsum, "Lorem
+                ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                The standard chunk of Lorem Ipsum used since the 1500s is
+                reproduced below for those interested. Sections 1.10.32 and
+                1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
+                reproduced in their exact original form, accompanied by English
+                versions from the 1914 translation by H. Rackham. Where can I
+                get some? There are many variations of passages of Lorem Ipsum
+                available, but the majority have suffered alteration in some
+                form, by injected humour, or randomised words which don't look
+                even slightly believable. If you are going to use a passage of
+                Lorem Ipsum, you need to be sure there isn't anything
+                embarrassing hidden in the middle of text. All the Lorem Ipsum
+                generators on the Internet tend to repeat predefined chunks as
+                necessary, making this the first true generator on the Internet.
+                It uses a dictionary of over 200 Latin words, combined with a
+                handful of model sentence structures, to generate Lorem Ipsum
+                which looks reasonable. The generated Lorem Ipsum is therefore
+                always free from repetition, injected humour, or
+                non-characteristic words etc.
+              </Dialog>
             </label>
           </div>
 
